99629881e2d746107b19d18ae2df289dfca5536d
chibi-bibibi <mk.i-l-m.96@ezweb.ne.jp>
2023-12-13 00:31:10 +0900

    c

--- app/playing/contents.tsx
+++ app/playing/contents.tsx
@@ -13,7 +13,7 @@ const Contents = () => {
           </div>
           <Image
             className="p-8"
-            src={'/../../../public/playing/choju74_0001_0.png'}
+            src={'/../../public/playing/choju74_0001_0.png'}
             width={200}
             height={200}
             alt="Playing panda"
