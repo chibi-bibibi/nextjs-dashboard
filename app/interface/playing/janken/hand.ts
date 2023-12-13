26de89a2427adea1fc32f4b3615f50ff367613cb
chibi-bibibi <mk.i-l-m.96@ezweb.ne.jp>
2023-12-13 22:17:30 +0900

    x

--- app/interface/playing/janken/hand.ts
+++ app/interface/playing/janken/hand.ts
@@ -12,19 +12,19 @@ export const hands = [
   {
     id: 1,
     text: 'グー',
-    playerImage: `${dir}/playing/janken/zyanken_nekonogu-.png`,
-    npcImage: `${dir}/playing/janken/doubutu_tanukinozyanken_gu.png`,
+    playerImage: `/playing/janken/zyanken_nekonogu-.png`,
+    npcImage: `/playing/janken/doubutu_tanukinozyanken_gu.png`,
   },
   {
     id: 2,
     text: 'チョキ',
-    playerImage: `${dir}/playing/janken/zyanken_nekonotyoki.png`,
-    npcImage: `${dir}/playing/janken/doubutu_tanukinozyanken_tyoki.png`,
+    playerImage: `/playing/janken/zyanken_nekonotyoki.png`,
+    npcImage: `/playing/janken/doubutu_tanukinozyanken_tyoki.png`,
   },
   {
     id: 3,
     text: 'パー',
-    playerImage: `${dir}/playing/janken/zyanken_nekonopa-.png`,
-    npcImage: `${dir}/playing/janken/doubutu_tanukinozyanken_pa.png`,
+    playerImage: `/playing/janken/zyanken_nekonopa-.png`,
+    npcImage: `/playing/janken/doubutu_tanukinozyanken_pa.png`,
   },
 ];
