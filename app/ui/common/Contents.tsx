0e0e07e5c9752e8f279a435826d929187f882bd7
chibi-bibibi <mk.i-l-m.96@ezweb.ne.jp>
2023-12-13 21:49:25 +0900

    c

--- app/ui/common/Contents.tsx
+++ app/ui/common/Contents.tsx
@@ -11,7 +11,7 @@ const Contents = () => {
       <div className="overflow-hidden rounded border-2 border-red">
         <Link href="/playing">
           <Image
-            src={`${dir}/panda.png`}
+            src={dir+'/panda.png'.replace('undefined/', '')}
             width={200}
             height={200}
             alt="Playing panda"
