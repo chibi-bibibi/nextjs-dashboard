371d08451da00ca0f3d7b9d6cf2dcdfc350b37ce
chibi-bibibi <mk.i-l-m.96@ezweb.ne.jp>
2023-12-17 16:47:23 +0900

    フッター修正

--- app/ui/common/Footer.tsx
+++ app/ui/common/Footer.tsx
@@ -3,7 +3,7 @@ import styles from '@/app/ui/home.module.css';
 const Footer = () => {
   return (
     <footer className={`${styles.footer}`}>
-      &copy; 2022 bluecode, inc. All Rights Reserved.
+      &copy; 2023 chi All Rights Reserved.
     </footer>
   );
 };
